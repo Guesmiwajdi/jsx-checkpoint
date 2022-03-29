550e3a9ffa458904d0359701627c4bb930489cb3
Guesmiwajdi <99688242+Guesmiwajdi@users.noreply.github.com>
2022-03-29 23:06:58 +0100

    jsx-check

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import logo from './logo21.jpg'
 import './App.css';
-
+import ReactPlayer from 'react-player'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div style={{maxWidth:"100vw"}}>
+
+<h1 className="title red">checkpoint-jsx</h1>
+<br/ >
+<img src="/logo22.jpg" alt=" logo22" />
+<br/ >
+<img src={logo} alt ="logo21"/>
+</div>
+<ReactPlayer url="https://www.youtube.com/watch?v=9GtB5G2xGTY"/>
     </div>
   );
 }
